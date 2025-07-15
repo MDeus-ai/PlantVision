e1597d9aec256de724cf291d14ff3ce662aedddf
Muhumuza Deus <muhumuzadeus7@gmail.com>
2025-07-15 19:17:36 +0300

    feat: corrected test_transforms.py forming a dummy_pil_image

--- tests/data/test_transforms.py
+++ tests/data/test_transforms.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import numpy as np
+from PIL import Image
 from torchvision import transforms
 from PlantVision.data.transforms import get_transforms
 
@@ -23,11 +24,12 @@ def test_get_transforms_resizes_correctly():
     test_img_size = 64
 
     # A dummy tensor representing a 3-channel image of a different size
-    dummy_image_np = np.uint8(np.random.rand(100, 120, 3) * 255) # (channels, height, width)
+    dummy_image_np = np.uint8(np.random.rand(100, 120, 3) * 255)
+    dummy_pil_image = Image.fromarray(dummy_image_np)
 
     # ACT
     transform_pipeline = get_transforms(img_size=test_img_size)
-    transformed_image = transform_pipeline(dummy_image_np)
+    transformed_image = transform_pipeline(dummy_pil_image)
 
     # ASSERT: The output should be a tensor
     assert isinstance(transformed_image, torch.Tensor), "Transformed output should be a torch.Tensor"
